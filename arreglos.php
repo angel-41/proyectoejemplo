54e56be4a5b69fd59289ad7c7b72bddceb6e569b
miguel2896 <46729183+miguel2896@users.noreply.github.com>
2020-03-14 12:38:46 -0600

    descomentado

--- arreglos.php
+++ arreglos.php
@@ -1,11 +1,11 @@
 <?php 
 
-// $array = array(1, 2, 3, 4);
-// foreach ($array as &$valor) {
-//     $valor = $valor * 2;
-//      //var_dump($array);
-//      // unset($valor);
-// } 
+$array = array(1, 2, 3, 4);
+foreach ($array as &$valor) {
+    $valor = $valor * 2;
+     //var_dump($array);
+     // unset($valor);
+} 
 
 $array = array(
 	"uno"=>"Miguel", 
